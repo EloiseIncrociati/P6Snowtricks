222879532402f4059bca57b7fb3af57b89b7a67a
EloiseIncrociati <eloise.incrociati@myrem.fr>
2021-09-03 16:26:53 +0200

    Configuration Database

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccountController extends AbstractController
+class TricksController extends AbstractController
 {
     /**
-     * @Route("/account", name="account")
+     * @Route("/", name="homepage")
      */
     public function index(): Response
     {
-        return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
+        return $this->render('tricks/index.html.twig', [
+            'controller_name' => 'TricksController',
         ]);
     }
 }
