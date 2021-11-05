93bf92acae61e7bbaf123713e63dbdae36531855
EloiseIncrociati <eloise.incrociati@myrem.fr>
2021-11-05 08:29:13 +0100

    feat-user-account: Essayer de limiter les Controllers, déplacement du controller Login dans un seul controller Account. Paramétrage des controller Account et User. Rédaction fichiers twig

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Repository\CommentRepository;
+use App\Repository\TrickRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class AdminController extends AbstractController
+{
+    public function dashboard(TrickRepository $repoTrick, CommentRepository $repoComment): Response
+    {
+        $tricks = $repoTrick->findBy([], ['id' => 'DESC'], 4);
+        $comments = $repoComment->findBy([], ['id' => 'DESC'], 3);
+        return $this->render('admin/dashboard.html.twig', [
+            'tricks' => $tricks,
+            'comments' => $comments,
+        ]);
+    }
+}
